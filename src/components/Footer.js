85172156c2257390beb9af775590f208c24f35f6
kblyclkn <105425261+kblyclkn@users.noreply.github.com>
2023-05-23 16:59:35 +0300

    navbar and footer ui completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
 const Footer = () => {
-  return <h4>footer</h4>
+  return <Wrapper>
+    <h5>
+      &copy;{new Date().getFullYear()}
+      <span>Rahat Tembellik</span>
+    </h5>
+      <h5>
+        Tüm Hakları Saklıdır
+      </h5>
+  </Wrapper>
 }
 
 const Wrapper = styled.footer`
